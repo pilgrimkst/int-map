bedf06427d962b4e6b565b0d13a61a46e38b1331
Kuznetsov Andrey <andrey198701@gmail.com>
2015-04-07 17:41:39 +0700

    fixed entry search

--- src/main/java/IntToLongMapImpl.java
+++ src/main/java/IntToLongMapImpl.java
@@ -1,26 +1,29 @@
 import com.sun.istack.internal.Nullable;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 public class IntToLongMapImpl implements IntToLongMap {
     public static final int INITIAL_CAPACITY = 10;
+    private static final double MAX_LOAD_FACTOR = 2.0;
     private static final IntToLongEntry EMPTY_ENTRY = new IntToLongEntry(IntToLongMap.DEFAULT_KEY, IntToLongMap.DEFAULT_VALUE);
     public IntToLongEntry[] buckets = new IntToLongEntry[INITIAL_CAPACITY];
     private int capacity = INITIAL_CAPACITY;
     private int size = 0;
 
     public long get(int key) {
-        return entryByKey(key)
+        return entryByKey(key, buckets)
                 .orElse(EMPTY_ENTRY)
                 .getValue();
     }
 
     public long put(int key, long value) {
-        int index = getBucketIndexForKey(key);
-
+        int index = getBucketIndexForKey(key, capacity);
         IntToLongEntry bucket = buckets[index];
         if (bucket == null) {
             buckets[index] = new IntToLongEntry(key, value);
+//            rebalanceBucketsIfRequired();
             size++;
             return EMPTY_ENTRY.getValue();
         } else {
@@ -40,6 +43,28 @@ public class IntToLongMapImpl implements IntToLongMap {
         }
     }
 
+    private void rebalanceBucketsIfRequired() {
+        if (size / buckets.length >= MAX_LOAD_FACTOR) {
+            increaseBucketSizeTo(capacity * 2);
+        }
+    }
+
+    private void increaseBucketSizeTo(int newBucketSize) {
+        IntToLongEntry[] newBuckets = new IntToLongEntry[newBucketSize];
+        Set<IntToLongEntry> entrys = entrySet();
+
+    }
+
+    private Set<IntToLongEntry> entrySet() {
+        Set<IntToLongEntry> entrys = new HashSet<>();
+        for (IntToLongEntry e : buckets) {
+            do {
+                entrys.add(e);
+            } while (e.hasNext());
+        }
+        return entrys;
+    }
+
     private IntToLongEntry findLastEntryInBucket(IntToLongEntry bucket) {
         return !bucket.hasNext() ? bucket : findLastEntryInBucket(bucket.getNext());
     }
@@ -48,41 +73,30 @@ public class IntToLongMapImpl implements IntToLongMap {
         return size;
     }
 
-    private Optional<IntToLongEntry> entryByKey(int key) {
-        IntToLongEntry entry = getBucketByKey(key);
+    private Optional<IntToLongEntry> entryByKey(int key, IntToLongEntry[] buckets) {
+        IntToLongEntry entry = getBucketByKey(key, buckets);
         return entryByKey(entry, key);
     }
 
     private Optional<IntToLongEntry> entryByKey(IntToLongEntry startEntry, int key) {
-        if (startEntry != null) {
-            do {
-                if (startEntry.getKey() == key) {
-                    return Optional.of(startEntry);
-                }
-                startEntry = startEntry.next;
-            } while (startEntry.hasNext());
-        }
-
-        return Optional.empty();
+        if (startEntry == null) return Optional.empty();
+        return startEntry.getKey() == key ? Optional.of(startEntry) : entryByKey(startEntry.next, key);
     }
 
     @Nullable
-    private IntToLongEntry getBucketByKey(int key) {
-        int bucketNumber = getBucketIndexForKey(key);
+    private IntToLongEntry getBucketByKey(int key, IntToLongEntry[] buckets) {
+        int bucketNumber = getBucketIndexForKey(key, capacity);
         return buckets[bucketNumber];
     }
 
-    private int getBucketIndexForKey(int key) {
+    private int getBucketIndexForKey(int key, int capacity) {
         return key % capacity;
     }
 
     private static class IntToLongEntry {
-        private int key;
+        private final int key;
         private long value;
         private IntToLongEntry next;
-
-        public IntToLongEntry() {
-        }
 
         public IntToLongEntry(int key, long value) {
             this.key = key;
@@ -103,10 +117,6 @@ public class IntToLongMapImpl implements IntToLongMap {
 
         public int getKey() {
             return key;
-        }
-
-        public void setKey(int key) {
-            this.key = key;
         }
 
         public long getValue() {
